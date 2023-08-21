ce11be2c1bb5b9e749f104b12d2f83fdcff43237
Gustavo de los Santos <gusleo94@gmail.com>
2023-08-21 15:40:49 +0200

    updated

--- argos/energy_profile.r.1s+.sh
+++ argos/energy_profile.r.1s+.sh
@@ -3,3 +3,7 @@
 power_profile=$(system76-power profile | sed -n 1p)
 
 echo "$power_profile"
+echo "---"
+echo "Battery | bash='system76-power profile battery' terminal=false"
+echo "Balanced | bash='system76-power profile balanced' terminal=false"
+echo "Performance | bash='system76-power profile performance' terminal=false"
